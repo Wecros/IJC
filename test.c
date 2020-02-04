d5f033e57c52c9fccfe1ca2ce02e81d6619bc07b
Marek Filip <mafilip@192.168.0.104>
2020-02-04 22:30:34 +0100

    1st Lecture
    basics, finite state machine, gcc

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+const int MAX = 10;
+
+int main() {
+    // int i;
+    // for (i = 1; i <= 10; i++)
+    // {
+    //     printf("%f \n", 1/i);
+    // }
+
+    // printf("\xFF");
+
+    char c;
+    while ((c = getchar()) != EOF) {
+        putchar(c);
+    }
+    
+    return 0;
+}

d3c5e819a0d36dd9a719bd96692275c08cdb88dd
Wecros <wecros@gmail.com>
2020-03-05 14:44:19 +0100

    Setbit and Getbit finished

--- project-1/bitset.h
+++ project-1/bitset.h
@@ -3,9 +3,30 @@
 
 #define N 100
 
-typedef int * bitset_t; 
-
 typedef unsigned long bitset_index_t;
+typedef bitset_index_t * bitset_t; 
+
+// #define bitset_create(array, size)  ( size % (sizeof(bitset_index_t) * __CHAR_BIT__) ? \
+//     bitset_t array[size / (sizeof(bitset_index_t) * __CHAR_BIT__) + 2] : \
+//     bitset_t array[size / (sizeof(bitset_index_t) * __CHAR_BIT__) + 1] )
+
+#define bitset_create(array, size)  bitset_t bitarr[size / (sizeof(bitset_index_t) * __CHAR_BIT__) \
+    + ((size % (sizeof(bitset_index_t) * __CHAR_BIT__)) ? 2 : 1)] = {size}
+
+// #define bitset_create(array, size) do { \
+//     unsigned bitsize = sizeof(bitset_index_t) * __CHAR_BIT__; \
+//     bitset_t array[size / bitsize + bitsize % 32 ? 1 : 2]; \
+//     for (size_t i = 0; i < size / bitsize; i++) array[i] = 0; \
+//     array[0] = size; \
+// } while (0)
+
+// #define bitset_create(array, size) do { \
+//     unsigned bitsize = sizeof(bitset_index_t) * __CHAR_BIT__; \
+//     bitset_t array[size / bitsize + bitsize % 32 ? 1 : 2]; \
+//     for (size_t i = 0; i < size / bitsize; i++) array[i] = 0; \
+//     array[0] = size; \
+// } while (0)
+
 
 // #define bitset_create(array, size) ();
     //    definuje a _nuluje_ proměnnou jmeno_pole
@@ -17,7 +38,7 @@ typedef unsigned long bitset_index_t;
     //        bitset_create(q,100000L);    // q = pole 100000 bitů, nulováno
     //        bitset_create(q,-100);       // chyba při překladu
 
-#define bitset_alloc(array, size);
+// #define bitset_alloc(array, size);
     //    definuje proměnnou jmeno_pole tak, aby byla kompatibilní s polem
     //    vytvořeným pomocí bitset_create, ale pole bude alokováno dynamicky.
     //    Př: bitset_alloc(q,100000L); // q = pole 100000 bitů, nulováno
@@ -25,10 +46,10 @@ typedef unsigned long bitset_index_t;
     //    Pokud alokace selže, ukončete program s chybovým hlášením:
     //    "bitset_alloc: Chyba alokace paměti"
     
-#define bitset_free(array);
+// #define bitset_free(array);
     //    uvolní paměť dynamicky alokovaného pole
 
-#define bitset_size(array);
+// #define bitset_size(array);
     //    vrátí deklarovanou velikost pole v bitech (uloženou v poli)
 
 // #define bitset_setbit(array, index, value) (array[(index/32)] |= (value << (index % 32)))
@@ -43,22 +64,45 @@ typedef unsigned long bitset_index_t;
     //    Př: if(bitset_getbit(p,i)==1) printf("1");
     //        if(!bitset_getbit(p,i))   printf("0");
 
-void bitset_create(bitset_t array, int size) {
-    array = calloc(size, sizeof(bitset_t));
+
+// void bitset_alloc(bitset_t array, int size) {
+//     array = calloc(size, sizeof(bitset_t));
+    
+// }
+
+void bitset_free(bitset_t array) {
+    free(array);
+}
+
+unsigned long bitset_size(bitset_t array) {
+    return array[0];
 }
 
 void bitset_setbit(bitset_t array, bitset_index_t index, unsigned int value) {
-    int i = index / 32;
-    int pos = index % 32;
+    unsigned bitsize = sizeof(bitset_index_t) * __CHAR_BIT__;
+    value = value ? 1 : 0;
 
-    unsigned int flag = value << pos;
+    int i = (index / bitsize) + 1;
+    int pos = index % bitsize;
 
-    printf("index: %d, flag: %d\n", i, array[i] | flag);
+    unsigned flag = value;
+    flag = flag << pos;
+
     array[i] |= flag;
 }
 
-int bitset_getbit(bitset_t array, bitset_index_t index) {
-    int arrval = array[index / 32];
-    int bit = 1 & (arrval >> (index % 32));
+unsigned bitset_getbit(bitset_t array, bitset_index_t index) {
+    unsigned bitsize = sizeof(bitset_index_t) * __CHAR_BIT__;
+
+    int i = (index / bitsize) + 1;
+    int pos = index % bitsize;
+    int arrval = array[i];
+
+    unsigned long flag = 1;
+    flag = flag << pos;
+
+    // printf("idx: %lu, pos: %lu, flag: %lu, arr: %lu, BIT: %lu\n", i, pos, flag, arrval, arrval & flag);
+    
+    int bit = arrval & flag ? 1 : 0;
     return bit;
 }
--- project-1/eratosthenes.c
+++ project-1/eratosthenes.c
@@ -1,40 +1,70 @@
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 
 #include "bitset.h"
 
-void sieve(int el_count) {
-    bitset_t bitarr;
-    // bitset_create(bitarr, el_count);
-    // bitset_alloc(bitarr, el_count);
+#define BITSIZE 100
 
-    for (size_t i = 0; i < el_count; i++) {
-        bitarr[i] = 1;
-    } 
+void sieve(size_t el_count) {
+    (void) el_count;
+    // bitset_t bitarr[10];
+    // // bitset_create(bitarr, el_count);
+    // // bitset_alloc(bitarr, el_count);
 
-    // bitarr[0] = 1;
-    // bitarr[1] = 1;
+    // for (size_t i = 0; i < el_count; i++) {
+    //     bitarr[i] = 1;
+    // } 
 
-    for (size_t i = 0; i < sqrt(el_count); i++) {
-        if (bitarr[i]) {
-            for (size_t j = pow(i, 2); j < el_count; j += i) {
-                bitarr[j] = 0;
-            }
-        }
-    }
-    
+    // // bitarr[0] = 1;
+    // // bitarr[1] = 1;
+
+    // for (size_t i = 0; i < sqrt(el_count); i++) {
+    //     if (bitarr[i]) {
+    //         for (size_t j = pow(i, 2); j < el_count; j += i) {
+    //             bitarr[j] = 0;
+    //         }
+    //     }
+    // }
 }
 
 int main() {
-    bitset_t bitarr[10];
+    const unsigned long size = 3;
+    
 
-    for (bitset_index_t i = 0; i < 4; i++) {
+    bitset_create(bitarr, BITSIZE);
+
+    const size_t bitarr_len = sizeof(bitarr) / sizeof(bitarr[0]);
+    printf("bitarr len: %lu\n", bitarr_len);
+    // static_assert(bitarr_len == 3, "bitarr is not right");
+
+    for (size_t i = 0; i < bitarr_len; i++) {
+        printf("arr[%lu]: %lu\n", i, bitarr[i]);
+    }
+
+    for (bitset_index_t i = 0; i < 3; i++) {
         bitset_setbit(bitarr, i, 1);
     }
 
-    for (bitset_index_t i = 0; i < 10; i++) {
-        printf("val [%d]: %d\n", i, bitset_getbit(bitarr, i));
+    // bitset_setbit(bitarr, 1, 0);
+
+    for (bitset_index_t i = 64; i < 67; i++) {
+        bitset_setbit(bitarr, i, 1);
     }
+
+    // bitset_setbit(bitarr, 87, 1);
+    // bitset_setbit(bitarr, 87, 0);
+    // bitset_setbit(bitarr, 86, 1);
+    // bitset_setbit(bitarr, 88, 1);
+
+    for (bitset_index_t i = 0; i < 100; i++) {
+        printf("val [%lu]: %u\n", i, bitset_getbit(bitarr, i));
+    }
+
+    for (size_t i = 1; i < size; i++) {
+        printf("ultimate val: %lu\n", bitarr[i]);
+    }
+    printf("bitsize: %lu\n", bitset_size(bitarr));
 
     return 0;
 }
